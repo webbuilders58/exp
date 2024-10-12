4bf25798e3bbbe5c72d4de11b95c497f9b2b049c
webbuilders58 <109017934+webbuilders58@users.noreply.github.com>
2024-10-12 19:57:30 +0530

    Create exp4b.tcl

---
+++ exp4b.tcl
@@ -0,0 +1,37 @@
+#UDP
+set ns [new Simulator] 
+set nf [open udp.nam w]
+$ns namtrace-all $nf
+set tf [open out.tr w]
+$ns trace-all $tf
+proc finish {} {
+       	global ns nf tf
+	$ns flush-trace 
+	close $nf
+	close $tf
+	exec nam udp.nam & 
+	exit 0
+}
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+$ns duplex-link $n0 $n4 1Mb 50ms DropTail
+$ns duplex-link $n1 $n4 1Mb 50ms DropTail
+$ns duplex-link $n2 $n5 0.1Mb 1ms DropTail
+$ns duplex-link $n3 $n5 1Mb 1ms DropTail
+$ns duplex-link $n4 $n5 1Mb 50ms DropTail
+$ns duplex-link-op $n2 $n5 queuePos 0
+set tcp [new Agent/UDP]
+$ns attach-agent $n0 $tcp
+set sink [new Agent/Null]
+$ns attach-agent $n2 $sink
+$ns connect $tcp $sink
+set ftp [new Application/Traffic/CBR]
+$ftp attach-agent $tcp
+$ns at 0.0 "$ftp start"
+$ns at 2.5 "$ftp stop"
+$ns at 3 "finish"
+$ns run
